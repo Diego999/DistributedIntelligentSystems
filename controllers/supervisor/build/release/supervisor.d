a835dd5fca5287e90f7c57a8118ac77adc8f3fb5
Diego Antognini <diegoantognini@gmail.com>
2015-11-12 17:20:03 +0100

    Initial commit

---
+++ controllers/supervisor/build/release/supervisor.d
@@ -0,0 +1,6 @@
+build/release/supervisor.o: supervisor.c \
+  /Applications/Webots/include/controller/c/webots/robot.h \
+  /Applications/Webots/include/controller/c/webots/types.h \
+  /Applications/Webots/include/controller/c/webots/nodes.h \
+  /Applications/Webots/include/controller/c/webots/emitter.h \
+  /Applications/Webots/include/controller/c/webots/supervisor.h
