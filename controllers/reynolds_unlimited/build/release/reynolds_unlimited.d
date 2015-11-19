dd41f5e810e71ba6a32efecf53cf734ae00c9527
Doy <damien.doy@gmail.com>
2015-11-19 14:10:57 +0100

    modified gitignore to ignore executable/compile files

--- controllers/reynolds_unlimited/build/release/reynolds_unlimited.d
+++
@@ -1,8 +0,0 @@
-build/release/reynolds_unlimited.o: reynolds_unlimited.c \
-  /Applications/Webots/include/controller/c/webots/robot.h \
-  /Applications/Webots/include/controller/c/webots/types.h \
-  /Applications/Webots/include/controller/c/webots/nodes.h \
-  /Applications/Webots/include/controller/c/webots/differential_wheels.h \
-  /Applications/Webots/include/controller/c/webots/distance_sensor.h \
-  /Applications/Webots/include/controller/c/webots/emitter.h \
-  /Applications/Webots/include/controller/c/webots/receiver.h
--- controllers/supervisor/build/release/supervisor.d
+++
@@ -1,6 +0,0 @@
-build/release/supervisor.o: supervisor.c \
-  /Applications/Webots/include/controller/c/webots/robot.h \
-  /Applications/Webots/include/controller/c/webots/types.h \
-  /Applications/Webots/include/controller/c/webots/nodes.h \
-  /Applications/Webots/include/controller/c/webots/emitter.h \
-  /Applications/Webots/include/controller/c/webots/supervisor.h
